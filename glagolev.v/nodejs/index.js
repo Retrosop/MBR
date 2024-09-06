5034ee8633796b53e8c68ea970f3e5acdfd8c763
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-09-07 08:53:12 +1000

    Добавляем сервер определение текущей даты

--- glagolev.v/nodejs/index.js
+++ glagolev.v/nodejs/index.js
@@ -1,8 +1,23 @@
+// const http = require("http");
+// http
+//   .createServer(function (require, response) {
+//     response.end("Hello Nodejs");
+//   })
+//   .listen(3000, "127.0.0.1", function () {
+//     console.log("Сервер успешно запушены");
+//   });
+
 const http = require("http");
-http
-  .createServer(function (require, response) {
-    response.end("Hello Nodejs");
-  })
-  .listen(3000, "127.0.0.1", function () {
-    console.log("Сервер успешно запушены");
-  });
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain; charset=utf-8"); // Устанавливаем кодировку UTF-8
+  const currentDate = new Date().toLocaleString(); // Получаем текущую дату
+  res.end(`Текущая дата: ${currentDate}\n`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
+});
