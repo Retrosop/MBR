6d06e0a4f6b646ea0f0e747926418f431235aa0d
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-09-14 10:08:09 +1000

    Добавлено чтение файлов в nodejs

--- glagolev.v/nodejs/index.js
+++ glagolev.v/nodejs/index.js
@@ -41,7 +41,69 @@ const Goods = require("./goods.js");
 const tv = new Goods(10000, 54, 40);
 tv.print();
 
+//Глава 2.3 Объект global
 const greeting23 = require("./greeting23");
 global.username = "Eugene";
 global.console.log(date);
 greeting23.printMessage();
+
+//Глава 2.4 Передача параметров приложения
+//node index.js Tom 18
+const nodePath = process.argv[0];
+const appPath = process.argv[1];
+const username = process.argv[2];
+const userage = process.argv[3];
+
+console.log("nodePath:", nodePath);
+console.log("appPath:", appPath);
+console.log();
+console.log("name:", username);
+console.log("age:", userage);
+
+//Глава 2.5 Установка сторонних модулей
+//npm info lodash
+const lodash = require("lodash");
+
+const people = ["Tom", "Sam", "Bob"];
+const employees = ["Tom", "Alice", "Sam"];
+
+// объединение массивов - в результате только уникальные значения
+const result1 = lodash.union(people, employees);
+console.log(result1); // [ "Tom", "Sam", "Bob", "Alice" ]
+
+// пересечение массивов - в результате только общие значения
+const result2 = lodash.intersection(people, employees);
+console.log(result2); // [ "Tom", "Sam" ]
+
+//Глава 2.6 Конфигурация
+const express = require("express"); // получаем модуль express
+// создаем приложение express
+const app = express();
+
+// устанавливаем обработчик для маршрута "/"
+app.get("/", function (_, response) {
+  response.end("<html><body> <b>Hello world3!!!<b></body></html>");
+});
+// начинаем прослушивание подключений на 3000 порту
+app.listen(3000, function () {
+  console.log("Сервер начал принимать запросы по адресу http://localhost:3000");
+});
+
+//Глава 2.7
+//npm install -g nodemon
+
+//Глава 2.8
+const fs = require("fs");
+
+fs.readFile("hello.txt", function (error, data) {
+  if (error) {
+    // если возникла ошибка
+    return console.log(error);
+  }
+  console.log(data.toString()); // выводим считанные данные
+});
+console.log("Асинхронное чтение файлов");
+
+const data = fs.readFileSync("hello.txt");
+console.log(data.toString()); // выводим считанные данные
+console.log("Синхронное чтение файлов");
